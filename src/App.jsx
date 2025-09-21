fc92503091462d6aa045e8533e6070f543959979
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-21 15:07:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Personalinfoform from "./components/personalinfoform";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <Personalinfoform />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Personalinfoform.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import "./Personalinfoform.css";
+const personalinfoform = () => {
+  const [form, setInfo] = useState({ name: "", password: "", email: "" });
+
+  const ChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setInfo((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handlersubmit = async (e) => {
+    e.preventDefault();
+    console.log(form);
+    try {
+      await fetch("https://userform-qu82.onrender.com/form", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(form),
+      });
+      setInfo({ name: "", password: "", email: "" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handlersubmit}>
+        <input
+          type="text"
+          placeholder="enter something"
+          name="name"
+          onChange={ChangeHandler}
+          value={form.name}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="enter password"
+          name="password"
+          onChange={ChangeHandler}
+          value={form.password}
+          required
+        />
+
+        <input
+          type="email"
+          placeholder="enter email"
+          name="email"
+          onChange={ChangeHandler}
+          value={form.email}
+          required
+        />
+        <div className="formBtn">
+          <button type="submit" className="submit">
+            submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default personalinfoform;
